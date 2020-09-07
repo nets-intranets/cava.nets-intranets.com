edd44d970a8fb1c8371397f2ebfb143d7f17fcf3
Niels Gregers Johansen <ngjoh@nets.eu>
2020-09-07 10:29:58 +0200

    Updates

---
+++ precache-manifest.dce1e7c4d3fdad5e8d3fd32f18fd4f84.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "18c304149e9fef43d2fbbb6d8acb8d99",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9795448ac5d11e30c9d7",
+    "url": "/static/css/2.59636352.chunk.css"
+  },
+  {
+    "revision": "0c2613696fddbc9fcc26",
+    "url": "/static/css/main.439eb715.chunk.css"
+  },
+  {
+    "revision": "9795448ac5d11e30c9d7",
+    "url": "/static/js/2.e08efa32.chunk.js"
+  },
+  {
+    "revision": "e71a02d10f9e1e4321ed1b5d85e17404",
+    "url": "/static/js/2.e08efa32.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0c2613696fddbc9fcc26",
+    "url": "/static/js/main.61651534.chunk.js"
+  },
+  {
+    "revision": "d56d451c267610d1d9ee",
+    "url": "/static/js/runtime-main.9799d87c.js"
+  }
+]);
